7dfb8038672c88abcd3ea21ebca4763d8f5144ea
titof <hktitof@gmail.com>
2022-06-15 14:30:33 +0100

    initilized the project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  
+  return (
+    <>
+      <div>Hello word! By Titof</div>
+      <p>current Project is set up with Next js,React js,Typescript, Tailwind css, storybook and webpack</p>  
+    </>
+  )
+}
